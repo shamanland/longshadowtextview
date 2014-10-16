10687cb803ad9ea991b9b0d0d23e6b6024e4473f
Oleksii Kropachov <o.kropachov@shamanland.com>
2014-10-17 00:29:35 +0300

    Renamed package to com.shamanland

--- app/src/main/java/com/shamanland/longshadowtextview/LongShadowTextView.java
+++ app/src/main/java/com/shamanland/longshadowtextview/LongShadowTextView.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.shamanland.longshadowtextview;
 
 import android.content.Context;
 import android.graphics.Bitmap;
--- app/src/main/java/com/shamanland/longshadowtextview/MainActivity.java
+++ app/src/main/java/com/shamanland/longshadowtextview/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.shamanland.longshadowtextview;
 
 import android.app.Activity;
 import android.graphics.Color;
